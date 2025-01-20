105988805bf5437fa76180ba234d31a2a59723fb
Csongoranevem <czakocsongor@turr.hu>
2025-01-20 20:11:04 +0100

    IK-21 Mobil menu elkészítése

--- main.js
+++ main.js
@@ -62,13 +62,25 @@ document.querySelector('#RangeSlider .range-slider-input-right').addEventListene
 	hamburger.classList.toggle('open');
 	mobilMenu.classList.toggle('visible');
   
-	console.log(hamburger.classList.contains('open'));
-  });
-  //aloldalak
-  document.querySelector('.lenyit').addEventListener('click', () => {
-	const hAloldal = [document.querySelector('.DropdownEgesz')];	
-	});
-	hAloldal.style.display = (hAloldal.style.display === 'block') ? 'none' : 'block';  
 	//console.log(hamburger.classList.contains('open'));
   });
+  //aloldalak
+	document.querySelector('.lenyit').addEventListener('click', () => {
+		const hAloldal = document.querySelectorAll('.menuItemDropdown1');
+
+		hAloldal.forEach(e => {
+			e.style.display = (e.style.display === 'block') ? 'none' : 'block';
+
+		});
+		});
+
+	document.querySelector('.lenyit2').addEventListener('click', () => {
+		const hAloldal2 = document.querySelectorAll('.menuItemDropdown2');
+
+		hAloldal2.forEach(e => {
+			e.style.display = (e.style.display === 'block') ? 'none' : 'block';
+
+		});
+		});
+	//console.log(hamburger.classList.contains('open'));
 
